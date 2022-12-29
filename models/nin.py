b237d2dd4105621a03e93177ffb0c23e5d3987f8
User3574 <ro.machacek0@gmail.com>
2022-12-29 08:38:19 +0100

    CHANGE: NIN include Logits

--- models/nin.py
+++ models/nin.py
@@ -5,47 +5,36 @@ class NIN(nn.Module):
     def __init__(self, num_classes):
         super(NIN, self).__init__()
         self.num_classes = num_classes
-
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 192, 5, padding=2),
+        self.classifier = nn.Sequential(
+            nn.Conv2d(3, 192, kernel_size=5, stride=1, padding=2),
             nn.ReLU(inplace=True),
-            nn.Conv2d(192, 160, 1),
+            nn.Conv2d(192, 160, kernel_size=1, stride=1, padding=0),
             nn.ReLU(inplace=True),
-            nn.Conv2d(160, 96, 1),
+            nn.Conv2d(160, 96, kernel_size=1, stride=1, padding=0),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(3, stride=2, ceil_mode=True),
-            nn.Dropout(inplace=True),
-
-            nn.Conv2d(96, 192, 5, padding=2),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+            nn.Dropout(0.5),
+            nn.Conv2d(96, 192, kernel_size=5, stride=1, padding=2),
             nn.ReLU(inplace=True),
-            nn.Conv2d(192, 192, 1),
+            nn.Conv2d(192, 192, kernel_size=1, stride=1, padding=0),
             nn.ReLU(inplace=True),
-            nn.Conv2d(192, 192, 1),
+            nn.Conv2d(192, 192, kernel_size=1, stride=1, padding=0),
             nn.ReLU(inplace=True),
-            nn.AvgPool2d(3, stride=2, ceil_mode=True),
-            nn.Dropout(inplace=True),
-
-            nn.Conv2d(192, 192, 3, padding=1),
+            nn.AvgPool2d(kernel_size=3, stride=2, padding=1),
+            nn.Dropout(0.5),
+            nn.Conv2d(192, 192, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(192, 192, 1),
+            nn.Conv2d(192, 192, kernel_size=1, stride=1, padding=0),
             nn.ReLU(inplace=True),
-            nn.Conv2d(192, self.num_classes, 1),
+            nn.Conv2d(192, 10, kernel_size=1, stride=1, padding=0),
             nn.ReLU(inplace=True),
-            nn.AvgPool2d(8, stride=1)
+            nn.AvgPool2d(kernel_size=8, stride=1, padding=0),
         )
-        self._initialize_weights()
 
     def forward(self, x):
-        x = self.features(x)
+        x = self.classifier(x)
         x = x.view(x.size(0), self.num_classes)
         return x
-
-    def _initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                m.weight.data.normal_(0, 0.05)
-                if m.bias is not None:
-                    m.bias.data.zero_()
 
 
 def nin(num_classes=10):
